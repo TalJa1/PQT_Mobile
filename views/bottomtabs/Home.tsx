9b613dfb4a97efb1ac59b517f24aadd6820ea155
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2025-05-16 10:35:53 +0700

    Add StatusBar component to Home for improved UI consistency

--- views/bottomtabs/Home.tsx
+++ views/bottomtabs/Home.tsx
@@ -1,10 +1,11 @@
-import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import {ScrollView, StatusBar, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 
 const Home = () => {
   return (
     <SafeAreaView style={styles.container}>
+      <StatusBar barStyle="light-content" backgroundColor={'#C9E5FF'} />
       <ScrollView>
         <View>
           <Text>Home</Text>
