414f6a48722a3649ccf2613fb1517c431ccdb1ae
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2025-05-17 14:35:38 +0700

    Enhance tab bar styling with improved shadow effects for better visibility

--- App.tsx
+++ App.tsx
@@ -58,6 +58,11 @@ const App = () => {
             left: 0,
             right: 0,
             bottom: 0,
+            shadowColor: '#FFFFFF',
+            shadowOffset: {width: 3, height: -3},
+            shadowOpacity: 0.7,
+            shadowRadius: 3,
+            elevation: 20,
           },
           headerShown: false,
         }}>
