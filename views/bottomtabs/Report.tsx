2a5b251ffc2f0256c8c142802f135c82d012834c
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2025-05-18 10:48:47 +0700

    feat: Add CustomStatusBar component to Report screen for improved status visibility

--- views/bottomtabs/Report.tsx
+++ views/bottomtabs/Report.tsx
@@ -1,10 +1,12 @@
 import {ScrollView, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import CustomStatusBar from '../../components/CustomStatusBar';
 
 const Report = () => {
   return (
     <SafeAreaView style={styles.container}>
+      <CustomStatusBar backgroundColor="#fff" barStyle="dark-content" />
       <ScrollView>
         <View>
           <Text>Report</Text>
